6aa4b81540a23f4241847c942a747458cf198564
Colin Savage <csavag@unc.edu>
2023-01-19 08:46:06 -0500

    wrote ex01

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,33 @@
+"""EX01 - Chardle - A cute step toward Wordle"""
+
+__author__ = "730552002"
+
+word: str = input("Enter a 5-character word: ")
+if len(word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+char: str = input("Enter a single character: ")
+if len(char) != 1:
+    print("Error: Character must be a single character")
+    exit()
+count: int = 0
+print("Searching for " + char + " in " + word)
+if word[0] == char:
+    print(char + " found at index 0")
+    count = count + 1
+if word[1] == char:
+    print(char + " found at index 1")
+    count = count + 1
+if word[2] == char:
+    print(char + " found at index 2")
+    count = count + 1
+if word[3] == char:
+    print(char + " found at index 3")
+    count = count + 1
+if word[4] == char:
+    print(char + " found at index 4")
+    count = count + 1
+if count > 0:
+    print(str(count) + " instance of " + char + " found in " + word)
+else:
+    print("No instances of " + char + " found in " + word)
