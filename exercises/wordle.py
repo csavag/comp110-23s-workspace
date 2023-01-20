3898204624efdded4285c680f920144af18c9be8
Colin Savage <csavag@unc.edu>
2023-01-20 12:46:52 -0500

    wordle

--- exercises/wordle.py
+++ exercises/wordle.py
@@ -1,3 +1,26 @@
 import random
 import pathlib
-print(open("words.txt", "r"))
+words = (open("words.txt", "r"))
+lst = words.read().split("\n")
+board = ["|_|"] * 5
+below = [" x "] * 5
+print(board)
+print(below)
+word = lst[random.randint(0,495)]
+print("Input a five-letter word")
+guess = input().lower()
+while len(guess) != 5:
+    print("Five letters. Try again.")
+    guess = input().lower()
+"""for space in board:
+    board[board.index(space)] = ("|" + guess[board.index(space)] + "|")
+    if word[board.index(space)] == guess[board.index(space)]:
+        below[board.index(space)] = (" o ")
+    else:
+        if guess[board.index(space)] in word:
+            below[board.index(space)] = (" / ")
+        else:
+            below[board.index(space)] = (" x ")"""
+print(board)
+print(below)    
+print(guess)    
