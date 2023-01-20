7d9c400b23d51c94e4582c6bacede2bf3c7a44a0
Colin Savage <csavag@unc.edu>
2023-01-20 14:14:14 -0500

    eh wordle

--- exercises/wordle.py
+++ exercises/wordle.py
@@ -1,26 +1,44 @@
 import random
 import pathlib
+global board
+global word
+global below
+global count
+def wordcheck(gues):
+    n = 0
+    correct_letters = 0
+    for space in board:
+        board[n] = ("|" + gues[n] + "|")
+        if word[n] == gues[n]:
+            below[n] = ("o")
+            correct_letters += 1
+        else:
+            if gues[n] in word:
+                below[n] = ("/")
+            else:
+                below[n] = ("x")
+        n += 1
+    stt = ""
+    for item in below:
+        stt += item
+    print(stt)
+    print("Tries: " + str(count) + "/6")
+    if correct_letters == 5:
+        print("Congratulations! You solved the Wordle in " + str(count) + " tries!")
+        exit()
 words = (open("words.txt", "r"))
 lst = words.read().split("\n")
 board = ["|_|"] * 5
-below = [" x "] * 5
-print(board)
-print(below)
+below = ["x"] * 5
+count = 1
+#here
 word = lst[random.randint(0,495)]
-print("Input a five-letter word")
-guess = input().lower()
-while len(guess) != 5:
-    print("Five letters. Try again.")
+while count < 7:
+    print("Input a five-letter word")
     guess = input().lower()
-"""for space in board:
-    board[board.index(space)] = ("|" + guess[board.index(space)] + "|")
-    if word[board.index(space)] == guess[board.index(space)]:
-        below[board.index(space)] = (" o ")
-    else:
-        if guess[board.index(space)] in word:
-            below[board.index(space)] = (" / ")
-        else:
-            below[board.index(space)] = (" x ")"""
-print(board)
-print(below)    
-print(guess)    
+    while len(guess) != 5:
+        print("Five letters. Try again.")
+        guess = input().lower()
+    wordcheck(guess)
+    count += 1
+print("You did not solve the Wordle. The correct word was " + word + ".")
