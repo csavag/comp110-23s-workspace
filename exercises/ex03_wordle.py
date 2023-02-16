1f94e62f3ac7f4aef8ea7ed7695547c81770ab68
Colin Savage <csavag@unc.edu>
2023-02-16 15:42:37 -0500

    auhg

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,8 +1,9 @@
 """EX03- Wordle with a semblance of structure."""
 __author__ = "730552002"
 
+
 def contains_char(word: str, letter: str) -> bool:
-    """Determines if letter is in word at any index"""
+    """Determines if letter is in word at any index."""
     assert len(letter) == 1
     n: int = 0
     while n < len(word):
@@ -11,8 +12,9 @@ def contains_char(word: str, letter: str) -> bool:
         n = n + 1
     return False
 
+
 def emojified(guess: str, secret: str) -> str:
-    """Returns a string of colored boxes for the word"""
+    """Returns a string of colored boxes for the word."""
     assert len(guess) == len(secret)
     result: str = ""
     n: int = 0
@@ -27,15 +29,17 @@ def emojified(guess: str, secret: str) -> str:
         n = n + 1
     return result
 
+
 def input_guess(expected_length: int) -> str:
-    """"Gets user guess or something"""
+    """"Gets user guess or something."""
     inp: str = input(f"Enter a {expected_length} character word: ")
     while len(inp) != expected_length:
         inp = input(f"That wasn't {expected_length} chars! Try again. ")
     return inp
 
+
 def main() -> None:
-    """The entrypoint of the program and the main game loop"""
+    """The entrypoint of the program and the main game loop."""
     turn: int = 1
     secret_word: str = "codes"
     running = True
@@ -50,6 +54,7 @@ def main() -> None:
         if turn == 7:
             print("X/6 - Sorry, try again tomorrow!")
             running = False
+    
     
 if __name__ == "__main__":
     main()
