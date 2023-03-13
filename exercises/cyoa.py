aab9786ab19614ba4c8476abef703fa41d44a4e1
Colin Savage <csavag@unc.edu>
2023-03-13 09:45:54 -0400

    preliminary readtime function

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -2,13 +2,20 @@
 __author__ = "730552002"
 import time 
 import random
-
+import sys
+global points
+points: int = 0
+global player
+player: str = ""
+global playing
+playing: int = 1
 
 def main() -> None:
-    global points
-    global player
-    points: int = 0
-    player: str = ""
+    pass
+    #global points
+    #global player
+    #points: int = 0
+    #player: str = ""
 
 
 def greet() -> None:
@@ -31,7 +38,7 @@ def greet() -> None:
     time.sleep(3)
     print("You awaken due to being violently shaken.")
     time.sleep(2)
-    print("\"Great hero! You have arrived! Please, tell me your name!\"")
+    print("\"Great hero! You have arrived! Please, tell me your name!\"(Input)")
     player = input()
     print("You sit up. It seems you have fallen from the sky and crashed through the roof of an old man's home. That's somewhat unusual.")
     time.sleep(8)
@@ -43,11 +50,25 @@ def choice_1() -> None:
     while choice != "1" or choice != "2" or choice != "3":
         print("Will you help the old man (1), kill the old man (2), or go back to sleep (3)?")
         choice = input()
+    if choice == "3":
+        print("You choose to roll over and go back to sleep.")
+        time.sleep(2.5)
+        print("\"What? No! You must save us!\"")
+        time.sleep(2)
+        print("But you just went to sleep.")
+        time.sleep(3)
+        playing = 0
+        sys.exit("You did not save anything.")
     if choice == "1":
-        pass
+        print("")
 
+def read_time(sentence):
+    print(sentence)
+    lstt = sentence.split()
+    time.sleep((len(lstt)/3))
 
 if __name__ == "__main__":
-    main()
-    greet()
-    choice_1()
+    while playing == 1:
+        main()
+        greet()
+        choice_1()
